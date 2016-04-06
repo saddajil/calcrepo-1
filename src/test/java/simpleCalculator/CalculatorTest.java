84a074e70b8ca5a22c46d1004b5f34d4e2442bcc
Administrator <saddajil@gmail.com>
2016-04-06 16:46:31 +0900

    multi 추가

--- src/test/java/simpleCalculator/CalculatorTest.java
+++ src/test/java/simpleCalculator/CalculatorTest.java
@@ -19,5 +19,11 @@ public class CalculatorTest {
 		calc.sub(10, 20);
 		assertEquals(-10, calc.getResult());
 	}
+	@Test
+	public void testMulti() {
+		Calculator calc = new Calculator();
+		calc.multi(10, 20);
+		assertEquals(200, calc.getResult());
+	}
 
 }
